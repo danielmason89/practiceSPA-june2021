3dbf81ae6bc536988ceb4a65e9534ac041789e76
Daniel Mason <danmasson0@gmail.com>
2021-07-29 17:43:53 -0400

    finished practice stateless spa

---
+++ components/Header.js
@@ -0,0 +1,2 @@
+import html from "html-literal";
+export default () => html`<header> <h1> Example SPA 2021 </h1> </header>`;
---
+++ components/Main.js
@@ -0,0 +1,4 @@
+import html from "html-literal";
+import * as views from "./views"
+export default () => html `  ${views["Home"]()} ${views["Bio"]()} ${views["Gallery"]()}
+${views["Register"]()}`;
