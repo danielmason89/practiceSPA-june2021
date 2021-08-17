7acb69d6636129b59d9f92325e47d376a0c09ff6
Daniel Mason <danmasson0@gmail.com>
2021-08-17 17:13:40 -0400

    continued to work in spa, to develop backend

--- server/index.js
+++ server/index.js
@@ -1,9 +1,36 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
 
+const greetings = require("./routers/greetings");
+const pizzas = require("./routers/pizzas");
+
+dotenv.config();
 
 // Import can be here up ^^^^^
 //  Instantiation
 const app = express();
+
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', console.log.bind(console, 'Successfully opened connection to Mongo!'));
+
+// define middleware functions
+const logging = (request, response, next) => {
+  console.log(` ${request.method} ${request.url} ${Date.now()} `);
+  next();
+};
+
+// using the middleware
+app.use(express.json());
+app.use(logging);
+
+app.use(greetings);
+app.use(pizzas);
+
+
 
 // Configuring express instance
 app.get("/status", (request, response) => {
@@ -19,12 +46,9 @@ response.send(JSON.stringify({ message: "No GET routes on route URI" }), 404);
        })
    ;
 
-   app
-  .route("/greet/:name")
-  .get((request, response) => {
-    const name = request.params.name;
-    response.status(418).json({ message: `Hello ${name}` });
-  });
+
 
 // executing the express (this must be last)
-app.listen(4040, () => console.log("Listening on port 4040"));
+const port = process.env.PORT || 4040;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
